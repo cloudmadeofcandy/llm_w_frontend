8596005ccd7459e3ad954ebf2681b2572a2bdc24
Quan Truong <69132936+cloudmadeofcandy@users.noreply.github.com>
2024-09-04 09:25:24 +0000

    initial commit

---
+++ chat_ui.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import time
+import local_llm_interface as lli
+
+st.title('Chat UI')
+
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+    st.session_state.messages.append({'role': 'assistant', 'content': 'Hello! How can I help you today?'})
+    
+for message in st.session_state.messages:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+
+if prompt := st.chat_input('Insert your questions here?'):
+    with st.chat_message('user'):
+        st.markdown(prompt)
+
+    st.session_state.messages.append({'role': 'user', 'content': prompt})
+
+with st.chat_message('assistant'):
+    llm_instance = lli.local_llm_interface()
+    response = st.write_stream(llm_instance.stream_single_questions(prompt))
+
+st.session_state.messages.append({'role': 'assistant', 'content': response})
+
---
+++ local_llm_interface.py
@@ -0,0 +1,33 @@
+import ollama
+import requests
+
+# Create a new instance of the OLLAMA class
+
+class local_llm_interface:
+
+    model = 'llama3.1'
+    options = {
+        'num_predict':-1,
+        'temperature':0.2,
+        'top_k':40,
+        'top_p':0.9
+    }
+
+    endpoint = '127.0.0.1:11434'
+
+    def __init__(self):
+        pass
+
+    def stream_single_questions(self, question):
+        stream = ollama.generate(self.model, 
+                               prompt=question, 
+                               options=self.options, 
+                               stream=True)
+        for chunk in stream:
+            yield chunk['response']
+    
+    def shoot_single_questions(self, question):
+        return ollama.generate(self.model, 
+                               prompt=question, 
+                               options=self.options)['response']
+    
